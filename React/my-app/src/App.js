a20e801e836e10634986625e022212b7e1c45a15
zzirco <zzirco@naver.com>
2024-09-03 21:50:31 +0900

    20240903

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,29 +1,33 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [money, setMoney] = useState(0);
+  const onChange = (event) => {
+    event.preventDefault();
+    setMoney(event.target.value);
+  }
   useEffect(() => {
-    console.log("I run only once.");
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
   }, []);
-  useEffect(() => {
-    console.log("I run when 'keyword' changes.");
-  }, [keyword]);
-  useEffect(() => {
-    console.log("I run when 'counter' changes.");
-  }, [counter]);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? <strong>Loading...</strong> : 
+        <select>
+          {coins.map((coin) => <option>{coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD </option>)}
+        </select>
+      }
+      <form>
+        <input onChange={onChange} type="number" placeholder="write your money"/>
+        <button>calc</button>
+      </form>
     </div>
   );
 }

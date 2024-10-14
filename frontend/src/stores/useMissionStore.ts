b984371ec1127a368dc57fd2e626873507dbc99e
yuan <uann97@gmail.com>
2024-10-14 17:38:42 +0900

    [fix] 미션 생성 후 아이 이름 유지

--- frontend/src/stores/useMissionStore.ts
+++ frontend/src/stores/useMissionStore.ts
@@ -11,20 +11,26 @@ interface ChildProfile {
 
 
 type ChildIdState = {
-  id: number | null; // 현재 선택된 아이의 ID
-  setChildId: (id: number) => void; // ID를 설정하는 함수
-  getChildId: () => number | null; // ID를 반환하는 함수
-  children: ChildProfile[]; // 아이 프로필을 저장하는 배열
-  setChildren: (children: ChildProfile[]) => void; // 아이 프로필을 설정하는 함수
-  getChildren: () => ChildProfile[]; // 아이 프로필을 반환하는 함수
+  id: number | null; 
+  name: string | null;
+  setChildId: (id: number) => void; 
+  getChildId: () => number | null; 
+  setChildName: (name: string) => void; 
+  getChildName: () => string | null; 
+  children: ChildProfile[]; 
+  setChildren: (children: ChildProfile[]) => void; 
+  getChildren: () => ChildProfile[];
 
 };
 
 const useMissionStore = create<ChildIdState>((set, get) => ({
   id: null, // 초기 상태
-  setChildId: (id: number) => set({id}), // 상태를 업데이트하는 함수
-  getChildId: () => get().id, // 상태에서 id 값을 반환하는 함수
-  children: [], // 아이 프로필 배열 초기화
+  name:null,
+  setChildId: (id: number) => set({id}), 
+  getChildId: () => get().id, 
+  setChildName: (name: string) => set({name}), 
+  getChildName: () => get().name, 
+  children: [], 
   setChildren: (children: ChildProfile[]) => set({children}), // 아이 프로필을 설정하는 함수
   getChildren: () => get().children, // 아이 프로필을 반환하는 함수
 
